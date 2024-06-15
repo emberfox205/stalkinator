45098a15feee6144c127e38206ae65827e59e63c
Phuc Doan <89180888+emberfox205@users.noreply.github.com>
2024-06-15 16:41:37 +0700

    reimplement with Threading

--- data_script.py
+++ data_script.py
@@ -1,33 +1,34 @@
 from libs.oauth_token_get import oauth_token_get
 from libs.coords_get import coords_get
 import time, socket
-from multiprocessing import Process, Manager
+import threading
 
-manager = Manager()
-latest_token = manager.Value('d', None)  # 'd' is the typecode for strings in this context
+latest_token = None
 
-def update_token(shared_token):
+def update_token():
+    global latest_token
     while True:
         # Update the shared token value
-        shared_token.value = oauth_token_get()
-        #print(shared_token.value)
+        latest_token = oauth_token_get()
+        #print(latest_token)
         time.sleep(250)
 
-def get_coords(shared_token):
+def get_coords():
+    global latest_token
     while True:
         ipv4_address = socket.gethostbyname(socket.gethostname())
-        #print("coord: ",shared_token.value)
-        coords_get(access_token=shared_token.value, url=f"http://{ipv4_address}:8080/data")
+        #print("coord: ",latest_token)
+        coords_get(access_token=latest_token, url=f"http://{ipv4_address}:8080/data")
         time.sleep(10)
 
 if __name__ == "__main__":
     # Pass the shared latest_token to both functions
-    p1 = Process(target=update_token, args=(latest_token,))
-    p2 = Process(target=get_coords, args=(latest_token,))
-    p1.start()
-    if not latest_token.value:
+    t1 = threading.Thread(target=update_token)
+    t2 = threading.Thread(target=get_coords)
+    t1.start()
+    while not latest_token:
         print("Waiting for token...")
         time.sleep(2)
-    p2.start()
-    p1.join()
-    p2.join()
+    t2.start()
+    t1.join()
+    t2.join()

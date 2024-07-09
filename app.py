75f91bab57831133855805a8e40cc2187c5b27f5
Herr Cult <10423041@student.vgu.edu.vn>
2024-07-09 22:29:28 +0700

    Update Danger Zone along with marker''s interval

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 import json
-from os import path, stat
-import socket, atexit, time
 from datetime import timedelta , datetime
 import sqlite3, json
-import subprocess
 from libs.get_places import get_user_coords, fetch_places, save_places_to_db, haversine
 from libs.alert_mailer import Mailer
 
@@ -18,18 +15,18 @@ app.config['SECRET_KEY'] = 'averysecretkey'
 db = SQLAlchemy(app)
 
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
-    tid = db.Column(db.String(100), nullable=True)
-    
+    tid = db.Column(db.String(100), nullable=True) 
     def __init__(self, email,password, tid):
         self.email = email
         self.tid = tid
         self.password = password
-
+        
+        
+# Function to check if the user is in the safe zone        
 def distance_safe(connect, cur, marker):
     try:
         cur.execute("SELECT lat, lon, safeRange FROM safeZone WHERE email = ? order by ID DESC Limit 1", (session['email'],))
@@ -51,13 +48,10 @@ def distance_safe(connect, cur, marker):
             else:
                 print(f"{session["email"]} is not in the safe zone")  
                 return "Safe", 0      
-    
-
+# Function to check if the user is in the danger zone
 def distance_danger(connect, cur, marker):
-    
     cur.execute("SELECT name, lat, lon, distance FROM geofence WHERE thing_id = ?", (session['thing_id'],))
     dangerZones = cur.fetchall()
-    #print("DANGER", [dict(i) for i in dangerZones]) 
     if not dangerZones:   
         print(f"{session["email"]} does not have dangerZone yet reload.")
         return None
@@ -65,7 +59,7 @@ def distance_danger(connect, cur, marker):
     for dangerZone in dangerZones:
         dangerZone = dict(dangerZone)
         distance = dangerZone["distance"]
-        if distance < 30:
+        if distance < 20:
             dangers.append(dangerZone)
     if dangers:
         print(f"{session["email"]} is in the danger zone")
@@ -73,8 +67,8 @@ def distance_danger(connect, cur, marker):
         print(f"{session["email"]} is not in the danger zone")
                    
     return "Danger", dangers
-
-def getdb(thing_id):
+# This function is to get the geofence from the database to display it on the map
+def getgeofence(thing_id):
     conn = sqlite3.connect("instance/stalkinator.db")
     cur = conn.cursor()  
     cur.execute('''CREATE TABLE IF NOT EXISTS geofence 
@@ -128,7 +122,6 @@ def login():
             flash("Already Logged in")
             return redirect(url_for("dashboard"))
         
-        
         return render_template('login.html')
     
 @app.route("/register" , methods = ["POST", "GET"])
@@ -165,7 +158,8 @@ def dashboard():
     if "email" in session:
         session["flag"] = 1
         thing_id = session.get('thing_id')
-        geofences = getdb(thing_id)
+        geofences = getgeofence(thing_id)
+        print(geofences)
         return render_template("index.html", geofences=geofences)
     else:
         return redirect(url_for("login"))
@@ -181,25 +175,22 @@ def data():
     # This handles requests to update safe zone
     if request.method == "POST" and all(isinstance(float(request.form.get(key)), float) for key in ["lat", "lon", "safeRange"]):
         
-        # try:
-            lat = request.form.get("lat")	
-            lon = request.form.get("lon")
-            safeRange = request.form.get("safeRange")
-            values = [lat, lon, safeRange, session['email']]
-            print(f"{session['email']}",safeRange)
-            cur.execute("""CREATE TABLE IF NOT EXISTS safeZone (ID INTEGER PRIMARY KEY AUTOINCREMENT, lat real, lon real, safeRange integer, email text UNIQUE) """)
-            cur.execute("""INSERT INTO safeZone (lat, lon, safeRange, email) VALUES (?, ?, ?, ?) ON CONFLICT (email) DO UPDATE SET lat=excluded.lat, lon=excluded.lon, safeRange=excluded.safeRange""", values)
-            connect.commit()
-        # except:
-        #     return "Error decoding JSON"
-        
+        lat = request.form.get("lat")	
+        lon = request.form.get("lon")
+        safeRange = request.form.get("safeRange")
+        values = [lat, lon, safeRange, session['email']]
+        print(f"{session['email']}",safeRange)
+        cur.execute("""CREATE TABLE IF NOT EXISTS safeZone (ID INTEGER PRIMARY KEY AUTOINCREMENT, lat real, lon real, safeRange integer, email text UNIQUE) """)
+        cur.execute("""INSERT INTO safeZone (lat, lon, safeRange, email) VALUES (?, ?, ?, ?) ON CONFLICT (email) DO UPDATE SET lat=excluded.lat, lon=excluded.lon, safeRange=excluded.safeRange""", values)
+        connect.commit()
+       
     # This handles periodical requests from Front-end to display the saved coords, and send emails
     elif request.method == "GET":
-
+        
         if request.args.get("index"):
             # Execute a query
             cur.execute("""CREATE TABLE IF NOT EXISTS Makers (ID INTEGER PRIMARY KEY AUTOINCREMENT, lat real, lon real, time string, thing_id) """)
-            cur.execute(f"SELECT lat, lon, time FROM Makers WHERE thing_id = '{session['thing_id']}' order by time desc LIMIT 5")
+            cur.execute(f"SELECT lat, lon, time FROM Makers WHERE thing_id = '{session['thing_id']}' order by time desc LIMIT 3")
             # Fetch all rows as dictionaries
             rows = cur.fetchall()
             markers = []
@@ -212,6 +203,7 @@ def data():
             if not hahaha: 
                 return json.dumps(hahaha)
             
+            # Mailer object to send emails to user if :
             mailer = Mailer()
             if check_safe := distance_safe(connect, cur, markers[0]):
                 if (check_safe[1] == 1 and session["flag"] == 0):
@@ -228,6 +220,8 @@ def data():
                     mailer.send(session["email"], check_danger)
                     print("sending mail")
             return json.dumps(hahaha)
+        
+        # Save safeZone to database
         if request.args.get("safeZone"):
             cur.execute("""CREATE TABLE IF NOT EXISTS safeZone (ID INTEGER PRIMARY KEY AUTOINCREMENT, lat real, lon real, safeRange integer, email text UNIQUE) """)
             cur.execute(f"SELECT lat, lon, safeRange FROM safeZone WHERE email = '{session['email']}'")
@@ -240,7 +234,9 @@ def data():
                 }) 
             else:
                 return {} 
-
+        if request.args.get("geofences"):
+            print(getgeofence(session['thing_id']))
+            return getgeofence(session['thing_id'])
     else:
         return "Method not supported"
 
@@ -250,3 +246,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=8080, debug=True)
+	
